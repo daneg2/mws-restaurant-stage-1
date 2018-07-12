9fb8fa3f5ce0cb6af1b4f3908abafbb614ad09b8
daneg2 <dane@g2design.co.za>
2018-07-12 09:34:58 +0200

    style: Move all media queries to the same section

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZGFuZXNtaXQiLCJhIjoiY2ppenk4eTU0MGM2czNwcGNoeXRia3g4ZCJ9.NVkErUdJ9YRzF3Tl_4T2rw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
